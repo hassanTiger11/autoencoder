bcc95d71ffb62261b6e291761cb4928a96767b55
ualguest <halnamer@email.arizon.edu>
2022-03-03 14:28:56 -0700

    no shuffle

--- load_imgs.py
+++ load_imgs.py
@@ -9,5 +9,5 @@ IMAGE_PATH = os.path.join(os.getcwd(), "mutant_plots")
 
 builder = tfds.ImageFolder(IMAGE_PATH)
 print(builder.info)  # num examples, labels... are automatically calculated
-ds = builder.as_dataset(split='train', shuffle_files=True)
+ds = builder.as_dataset(split='train', shuffle_files=False)
 tfds.show_examples(ds, builder.info)

e9855a78ccb81fcbe2977b4a8043f3aa35441da5
ualguest <halnamer@email.arizon.edu>
2022-03-03 14:52:06 -0700

    changes pp

--- pp.py
+++ pp.py
@@ -3,7 +3,7 @@ FOLDER_PATH = os.path.join(os.getcwd(), 'mutant_plots')
 def change_folder_names():
     
     folder = open(FOLDER_PATH, 'w+')
-    for f in folder:
+    for f in os.listdir(FOLDER_PATH):
         print(f)
 
 def change_file_names():

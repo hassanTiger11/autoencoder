138e773a09bb3899c6a42274ab1cb5348d884b7f
ualguest <halnamer@email.arizon.edu>
2022-03-03 14:59:39 -0700

    changes pp

--- pp.py
+++ pp.py
@@ -5,8 +5,13 @@ def change_folder_names():
     for f in os.listdir(FOLDER_PATH):
         folder = os.path.join(FOLDER_PATH, f)
         for chng_f in os.listdir(folder):
-            new_folder_name = f'ds_{chng_f}'
-            print(f'old: {chng_f}\nnew: {new_folder_name}')
+            old_folder_name = os.path.join(folder, chng_f)
+            new_folder_name =  os.path.join(folder, f'ds_{chng_f}')
+            os.rename(old_folder_name, new_folder_name)
+            for img in new_folder_name:
+                old_img_name = os.path.join(new_folder_name, img)
+                new_img_name = os.path.join(new_folder_name, f'img_{img}')
+                os.rename(old_img_name, new_img_name)
 
 
 def change_file_names():

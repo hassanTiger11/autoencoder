11e1ed9377d47f67a169685f82bb01d4c86132a4
Hassan <halnamer@email.arizona.edu>
2022-03-07 12:07:06 -0700

    edits to load_imgs

--- load_imgs.py
+++ load_imgs.py
@@ -1,3 +1,5 @@
+'''
+This file '''
 import numpy as np
 import os
 import PIL
@@ -5,9 +7,6 @@ import PIL.Image
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import pathlib
-IMAGE_PATH = os.path.join(os.getcwd(), "mutant_plots")
+DS_PATH = os.path.join(os.getcwd(), "mutant_plots")
 
-builder = tfds.ImageFolder(IMAGE_PATH)
-print(builder.info)  # num examples, labels... are automatically calculated
-ds = builder.as_dataset(split='train', shuffle_files=False)
-tfds.show_examples(ds, builder.info)
+ds, ds_info = tfds.load(DS_PATH)

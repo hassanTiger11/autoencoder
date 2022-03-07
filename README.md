510ba491ba59d21182f06fc672c8387144ba5fe9
Hassan <halnamer@email.arizona.edu>
2022-03-07 11:48:08 -0700

    edit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Auto Encoder to drop shodows from pics
+
+### Data source
+I am using data from Cyverse that I downloaded manually. 
+
+## Processing
+Tensorflow was screaming at me at because of the names of the files
+So I built pp.py to fix the issue with file names
+
--- pp.py
+++ pp.py
@@ -1,20 +1,27 @@
 import os
 FOLDER_PATH = os.path.join(os.getcwd(), 'mutant_plots')
-def change_folder_names():
+def change_folder_names(FOLDER_PATH = ''):
     
     for f in os.listdir(FOLDER_PATH):
         folder = os.path.join(FOLDER_PATH, f)
         for chng_f in os.listdir(folder):
-            old_folder_name = os.path.join(folder, chng_f)
-            new_folder_name =  os.path.join(folder, f'ds_{chng_f}')
-            os.rename(old_folder_name, new_folder_name)
-            for img in new_folder_name:
-                old_img_name = os.path.join(new_folder_name, img)
-                new_img_name = os.path.join(new_folder_name, f'img_{img}')
-                os.rename(old_img_name, new_img_name)
+            if (not img.startswith('ds_')): 
+                
+                old_folder_name = os.path.join(folder, chng_f)
+                new_folder_name =  os.path.join(folder, f'ds_{chng_f}')
+                print(f'#chnge folder {old_folder_name} -> {new_folder_name}')
+                os.rename(old_folder_name, new_folder_name)
+                change_file_names(new_folder_name = new_folder_name)
+            
 
 
-def change_file_names():
+def change_file_names(new_folder_name = ''):
+    for img in os.listdir(new_folder_name):
+                if (not img.startswith('img_')): 
+                    old_img_name = os.path.join(new_folder_name, img)
+                    new_img_name = os.path.join(new_folder_name, f'img_{img}')
+                    print(f'#chnge img {old_img_name} -> {new_img_name}')
+                    os.rename(old_img_name, new_img_name)
     return
 
-change_folder_names()
+change_folder_names(FOLDER_PATH = FOLDER_PATH)

227ef6dce4359e5485b8f457417bf747e32eb48f
Hassan <halnamer@email.arizona.edu>
2022-03-07 14:40:23 -0700

    architecture

--- load_imgs.py
+++ load_imgs.py
@@ -7,12 +7,23 @@ import PIL.Image
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import pathlib
+import json
+from tifffile import memmap
 DS_PATH = os.path.join(os.getcwd(), "mutant_plots")
+def create_filname_dict():
+    ds = {}
+    labels = os.listdir(DS_PATH)
+    for lbl in labels:
+        cwd = os.path.join(DS_PATH, lbl)
+        images = os.listdir(cwd)
+        for img in images:
+            if(lbl in ds):
+                ds[lbl].append(os.path.join(cwd, img))
+            else:
+                ds[lbl] = []
+                ds[lbl].append(os.path.join(cwd, img))
+    output = open('ds.json'm 'r+')
+    json.dump(ds, output, indent=3)
+    output.close()
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  DS_PATH,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  
-  )
+create_filname_dict()

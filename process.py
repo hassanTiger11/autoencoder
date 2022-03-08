f414c78fe34b62b919fa44a4e0fb6e32e5f343f5
ualguest <halnamer@email.arizon.edu>
2022-03-08 16:12:31 -0700

    fixes

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ from load_imgs import load_dataset
 
 ds = load_dataset()
 
+
 autoencoder = Autoencoder(64) 
 
 autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
@@ -11,5 +12,5 @@ autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 autoencoder.fit(ds, ds,
                 epochs=10,
                 shuffle=True,
-                validation_data=(x_test, x_test))
+                validation_data=(ds, ds))
 autoencoder.save('saved_models/autoencoder_0')
